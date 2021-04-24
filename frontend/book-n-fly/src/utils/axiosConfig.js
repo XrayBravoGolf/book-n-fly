b6c459afce1fb918dbcbdcc4ffbb122d16caf6e5
XrayBravoGolf <xuxiaoyao213@gmail.com>
2021-04-23 19:20:29 -0700

    FE lint

--- frontend/book-n-fly/src/utils/axiosConfig.js
+++ frontend/book-n-fly/src/utils/axiosConfig.js
@@ -1,56 +1,54 @@
-import axios from 'axios';
-import router from '@/router';
-import * as API from '@/api.js';
+import axios from 'axios'
+import router from '@/router'
+import * as API from '@/api.js'
 
 // axios instance configs
 const $myAxios = axios.create({
-  timeout: 30,
-});
-$myAxios.all = axios.all;
-
-
+  timeout: 30
+})
+$myAxios.all = axios.all
 
 $myAxios.interceptors.request.use(config => {
   // get authority except the login api
   if (config.url !== API.LOGIN) {
     if (window.sessionStorage.getItem('Authorization')) {
-      config.headers.Authorization = JSON.parse(window.sessionStorage.getItem('Authorization'));
+      config.headers.Authorization = JSON.parse(window.sessionStorage.getItem('Authorization'))
     } else {
-      console.warn('invalid authorization cookies!');
+      console.warn('invalid authorization cookies!')
     }
   }
-  return config;
-});
+  return config
+})
 
 // http status code = 401
-function backToLogin() {
-    window.sessionStorage.removeItem("authorization");
-    router.push('/login');
-  }
+function backToLogin () {
+  window.sessionStorage.removeItem('authorization')
+  router.push('/login')
+}
 
 // catch http response
 // integrate error handling
 $myAxios.interceptors.response.use(
   response => {
-   if (response.status === 401) {
+    if (response.status === 401) {
       // ticket error
-      console.warn("[response] user's authorization is expired, to login page");
-      backToLogin();
+      console.warn("[response] user's authorization is expired, to login page")
+      backToLogin()
     }
-    return response;
+    return response
   },
   // invalid response
   error => {
     if (error) {
       if (error.response && error.response.status === 401) {
         // ticket error
-        console.warn("user's authorization is expired, to login page");
-        backToLogin();
-      } 
-      return Promise.reject(error);
+        console.warn("user's authorization is expired, to login page")
+        backToLogin()
+      }
+      return Promise.reject(error)
     }
-    return Promise.reject(new Error('unknown error'));
+    return Promise.reject(new Error('unknown error'))
   }
-);
+)
 
-export const myAxios = $myAxios;
+export const myAxios = $myAxios
--- frontend/book-n-fly/vue.config.js
+++ frontend/book-n-fly/vue.config.js
@@ -1,14 +1,14 @@
-const proxyTarget = "http://localhost:3000"
+const proxyTarget = 'http://localhost:3000'
 module.exports = {
-    lintOnSave: true,
-    devServer: {
-        port: 5000,
-        proxy: {
-          '^/api': {
-            target: proxyTarget,
-            ws: true,
-            changeOrigin: true
-        }
+  lintOnSave: true,
+  devServer: {
+    port: 5000,
+    proxy: {
+      '^/api': {
+        target: proxyTarget,
+        ws: true,
+        changeOrigin: true
       }
     }
+  }
 }
